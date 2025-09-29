950b72dbeb9addbfdfd698fef96b576dcbc9b8dd
tukeimark2k-dev <tukeimark2k@gmail.com>
2025-09-29 17:24:44 +0300

    Add files via upload

---
+++ JAVA WORK/mathlearningtool/src/main/java/vu/mathlearningtool/Mathlearningtool.java
@@ -0,0 +1,19 @@
+
+package vu.mathlearningtool;
+import java.util.Scanner;
+public class Mathlearningtool {
+
+    public static void main(String[] args) {
+        Scanner save =new Scanner(Sytem.in);
+        System.out.println("What is\"7+9\"?");
+        int option =save.nextInt();
+        int answer =16;
+        if(option==answer){
+            System.out.println("true");
+        }
+        else
+            System.out.println("False");
+    }
+}
+        
+
